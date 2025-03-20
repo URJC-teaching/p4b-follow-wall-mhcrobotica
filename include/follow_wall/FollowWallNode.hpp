a9bbf3f95a84db157f9dd6f6127bdcfdb5af45b3
Hugo <h.garciao.2022@alumnos.urjc.es>
2025-03-20 10:46:57 +0100

    Suicida

--- include/follow_wall/FollowWallNode.hpp
+++ include/follow_wall/FollowWallNode.hpp
@@ -20,15 +20,17 @@ private:
     void go_state(State new_state);
 
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
 
     State state_;
     float front_distance_;
     float left_distance_;
+    float right_distance_;
+
 
     static constexpr float SPEED_LINEAR = 0.3f;
-    static constexpr float SPEED_ANGULAR = 0.3f;
+    static constexpr float SPEED_ANGULAR = 0.6f;
     static constexpr float DISTANCE_TO_WALL = 1.0;
     static constexpr float OBSTACLE_THRESHOLD = 0.5;
     static constexpr float MAX_WALL_DETECTION = 2.0;
--- src/follow_wall/FollowWallNode.cpp
+++ src/follow_wall/FollowWallNode.cpp
@@ -15,24 +15,37 @@ FollowWallNode::FollowWallNode()
 : Node("follow_wall"), state_(GO_STRAIGHT) {
     
     subscription_ = create_subscription<sensor_msgs::msg::LaserScan>(
-        "/scan", 10, std::bind(&FollowWallNode::scan_callback, this, _1));
-    publisher_ = create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+        "/scan_filtered", 10, std::bind(&FollowWallNode::scan_callback, this, _1));
+    vel_pub_ = create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
     
     timer_ = create_wall_timer(20ms, std::bind(&FollowWallNode::control_cycle, this));
 }
 
 void FollowWallNode::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-    front_distance_ = *std::min_element(msg->ranges.begin(), msg->ranges.begin() + 10);
-    front_distance_ = std::min(front_distance_, *std::min_element(msg->ranges.end() - 10, msg->ranges.end()));
-    left_distance_ = *std::min_element(msg->ranges.begin() + 80, msg->ranges.begin() + 100);
+    int size = msg->ranges.size();
+    
+    // Detección frontal
+    front_distance_ = *std::min_element(msg->ranges.begin(), msg->ranges.begin() + 30);
+    front_distance_ = *std::min_element(msg->ranges.end() - 30, msg->ranges.end());
+    
+    // Detección lateral izquierda
+    left_distance_ = *std::min_element(msg->ranges.begin() + size / 4 - 20, msg->ranges.begin() + size / 4 + 20);
+
+    // Detección lateral derecha
+    right_distance_ = *std::min_element(msg->ranges.begin() + 3 * size / 4 - 20, msg->ranges.begin() + 3 * size / 4 + 20);
+
+    // Debug para verificar valores
+    RCLCPP_INFO(this->get_logger(), "Front: %.2f, Left: %.2f, Right: %.2f", 
+                front_distance_, left_distance_, right_distance_);
 }
 
+
 void FollowWallNode::control_cycle() {
-    geometry_msgs::msg::Twist cmd;
+    geometry_msgs::msg::Twist out_vel;
     
     switch (state_) {
         case GO_STRAIGHT:
-            cmd.linear.x = SPEED_LINEAR;
+            out_vel.linear.x = SPEED_LINEAR;
             if (front_distance_ < OBSTACLE_THRESHOLD) {
                 go_state(AVOID_OBSTACLE);
             } else if (left_distance_ < MAX_WALL_DETECTION) {
@@ -41,8 +54,8 @@ void FollowWallNode::control_cycle() {
             break;
         
         case FOLLOW_WALL:
-            cmd.linear.x = SPEED_LINEAR;
-            cmd.angular.z = -(left_distance_ - DISTANCE_TO_WALL) * WALL_FOLLOW_KP;
+            out_vel.linear.x = SPEED_LINEAR;
+            out_vel.angular.z = -(left_distance_ - DISTANCE_TO_WALL) * WALL_FOLLOW_KP;
             if (front_distance_ < OBSTACLE_THRESHOLD) {
                 go_state(AVOID_OBSTACLE);
             } else if (left_distance_ > MAX_WALL_DETECTION) {
@@ -51,15 +64,17 @@ void FollowWallNode::control_cycle() {
             break;
         
         case AVOID_OBSTACLE:
-            cmd.linear.x = 0.0;
-            cmd.angular.z = SPEED_ANGULAR;
-            if (front_distance_ > OBSTACLE_THRESHOLD) {
+            out_vel.linear.x = 0.0;
+            out_vel.angular.z = SPEED_ANGULAR;
+            if (left_distance_ > MAX_WALL_DETECTION && front_distance_ > OBSTACLE_THRESHOLD) {
+                go_state(GO_STRAIGHT);
+            } else if (left_distance_ < MAX_WALL_DETECTION) {
                 go_state(FOLLOW_WALL);
             }
             break;
     }
     
-    publisher_->publish(cmd);
+    vel_pub_->publish(out_vel);
 }
 
 void FollowWallNode::go_state(State new_state) {
